c9dfb906dbbb4285e0af2ecfbf1cf092c6692d9f
Hamaco <hamaco@hamacbook.local>
2009-11-17 11:28:06 +0900

    add screenrc

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 CURRENT=`pwd`
 
-for file in .vim .vimrc .gvimrc .emacs .emacs.d .vimperator .vimperatorrc .zsh .zshrc .zshenv .gitconfig
+for file in .vim .vimrc .gvimrc .emacs .emacs.d .vimperator .vimperatorrc .zsh .zshrc .zshenv .gitconfig .screenrc
 do
 	rm -f ~/$file
 	ln -s $CURRENT/dot$file ~/$file
