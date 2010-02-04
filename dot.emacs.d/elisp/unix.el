0ba0c3d7b7f8b99e01c7116157f5bba47657aae9
hamaco <hamanaka.kazuhiro@gmail.com>
2010-02-04 12:37:39 +0900

    fontsize++

--- dot.emacs.d/elisp/unix.el
+++ dot.emacs.d/elisp/unix.el
@@ -2,7 +2,7 @@
 	(progn
 		(cond
 			((eq system-type 'gnu/linux)
-			 (set-default-font "DejaVu Sans Mono-8")
+			 (set-default-font "DejaVu Sans Mono-14")
 			 (set-fontset-font (frame-parameter nil 'font)
 												 'japanese-jisx0208
 												 '("YukarryAA" . "unicode-bmp"))
