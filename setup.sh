66a8436ef42f0f4687ef6dc3b8b0f8f7ab8f59e8
hamaco <hamanaka.kazuhiro@gmail.com>
2010-07-09 09:30:12 +0900

    vimshell: add vimshrc.

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 CURRENT=`pwd`
 
-for file in .vim .vimrc .gvimrc .emacs .emacs.d .vimperator .vimperatorrc .zsh .zshrc .zshenv .gitconfig .gitignore .screenrc
+for file in .vim .vimrc .vimshrc .gvimrc .emacs .emacs.d .vimperator .vimperatorrc .zsh .zshrc .zshenv .gitconfig .gitignore .screenrc
 do
 	rm -f ~/$file
 	ln -s $CURRENT/dot$file ~/$file
