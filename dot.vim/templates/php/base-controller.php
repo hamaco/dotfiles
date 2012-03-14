e6fa03acad94a4231efc98568c93bce561792995
hamaco <hamanaka.kazuhiro@gmail.com>
2012-03-15 02:16:18 +0900

    vim: Sabel ControllerのTemplateが微妙なのでちょい修正

--- dot.vim/templates/php/base-controller.php
+++ dot.vim/templates/php/base-controller.php
@@ -2,7 +2,5 @@
 
 class {{_expr_:substitute(substitute(substitute(expand('%:p'), '.*app/\(.\)', '\u\1', 'g'), '/controllers/', '_Controllers_', 'g'), '\.php$', '', 'g')}} extends {{_expr_:substitute(expand('%:p'), '^.*app/\(\w*\)/.*$', '\u\1', 'g')}}_Controllers_Base
 {
-  public function {{_cursor_}}()
-  {
-  }
+  {{_cursor_}}
 }
