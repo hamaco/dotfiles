5d9fb980309d0bfc259cbb46b8307107add4a209
hamaco <hamanaka.kazuhiro@gmail.com>
2012-09-16 21:47:20 +0900

    vim: SabelのFixture書くの面倒だからCSVでデータ管理するようにしてみた

--- dot.vim/templates/php/base-fixture.php
+++ dot.vim/templates/php/base-fixture.php
@@ -1,17 +1,32 @@
 <?php
 
-class Fixture_{{_name_}} extends Sabel_Test_Fixture
+Sabel::fileUsing(FIXTURE_DIR . DS . 'Base.php');
+
+class Fixture_{{_name_}} extends Fixture_Base
 {
   public function upFixture()
   {
-    $this->insert(array(
-      'id' => 1,
-      {{_cursor_}}
-    ));
-  }
+    $reader = new Csv_Reader(RUN_BASE . '/data/csv/{{_expr_:substitute(substitute(expand('%:t'), '\(.*\)\.php', '\l\1', 'g'), '\u', '_\l\0', 'g')}}.csv');
+    $reader->read();
 
-  public function downFixture()
-  {
-    $this->deleteAll();
+    $columns = array();
+    foreach ($reader->read() as $pos => $column_name) {
+      if ($column_name !== '') {
+        $columns[$column_name] = $pos;
+      }
+    }
+
+    while (($line = $reader->read()) !== false) {
+      if (!$line[1]) {
+        break;
+      }
+
+      $data = array();
+      foreach ($columns as $column_name => $pos) {
+        $data[$column_name] = $line[$pos];
+      }
+
+      $this->insert($data);
+    }
   }
 }
