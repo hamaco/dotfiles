a937c0bd8951fed7e6e73362f6529e3c8a422e64
hamaco <hamanaka.kazuhiro@gmail.com>
2012-10-04 02:54:36 +0900

    zsh: rbenv/phpenvなど$HOME/app以下のディレクトリにあるzshenvをロードするようにしてみた

--- zsh/zsh/main.zsh
+++ zsh/zsh/main.zsh
@@ -1,7 +1,17 @@
+
+function source_if() {
+	[[ -s "$1" ]] && source "$1"
+}
+
 ZSH_HOME="$HOME/.zsh"
 
 # load config
 for config_file ($ZSH_HOME/config/*.zsh)
 do
 	source $config_file
+done
+
+for dir in $HOME/app/*
+do
+	source_if $dir/zshenv
 done
