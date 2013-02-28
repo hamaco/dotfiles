ac9089afbe7f2287e9d04d0e1c8ad1c2f421669a
hamaco <hamanaka.kazuhiro@gmail.com>
2013-02-28 18:10:28 +0900

    zsh: 圧縮ファイル解凍用のsuffixパクった
    http://d.hatena.ne.jp/itchyny/20130227/1361933011

--- zsh/zsh/config/aliases.zsh
+++ zsh/zsh/config/aliases.zsh
@@ -115,7 +115,23 @@ alias -s txt=cat
 #alias -s php=vim
 alias -s log=less
 alias -s jar="java -jar"
-alias -s {gz,tgz,zip,lzh,bz2,tbz,Z,tar,arj,xz}=aunpack # aptitude install atool
+unction extract() {
+  case $1 in
+    *.tar.gz|*.tgz) tar xzvf $1;;
+    *.tar.xz) tar Jxvf $1;;
+    *.zip) unzip $1;;
+    *.lzh) lha e $1;;
+    *.tar.bz2|*.tbz) tar xjvf $1;;
+    *.tar.Z) tar zxvf $1;;
+    *.gz) gzip -dc $1;;
+    *.bz2) bzip2 -dc $1;;
+    *.Z) uncompress $1;;
+    *.tar) tar xvf $1;;
+    *.arj) unarj $1;;
+  esac
+}
+alias -s {gz,tgz,zip,lzh,bz2,tbz,Z,tar,arj,xz}=extract
+# alias -s {gz,tgz,zip,lzh,bz2,tbz,Z,tar,arj,xz}=aunpack # aptitude install atool
 
 case "${OSTYPE}" in
 linux*)
