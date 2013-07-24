5f910ba3c22fc949b943168f3dc63b43ab70c2f7
hamaco <hamanaka.kazuhiro@gmail.com>
2013-07-24 12:43:24 +0900

    zsh: もしかしたら便利かもなので Enter で ls + git status -sb するようにしてみた
    Enter連打した時の対策とかあったほうがよいかもしれない……

--- zsh/zsh/main.zsh
+++ zsh/zsh/main.zsh
@@ -95,7 +95,7 @@ autoload -U promptinit; promptinit
 
 
 # Functions: ============================================================== {{{1
-rationalise-dot() {
+function rationalise-dot() {
 	if [[ $LBUFFER = *.. ]]; then
 		LBUFFER+=/..
 	else
@@ -104,6 +104,62 @@ rationalise-dot() {
 }
 zle -N rationalise-dot
 bindkey . rationalise-dot
+
+
+function ls_abbrev() {
+    # -a : Do not ignore entries starting with ..
+    # -C : Force multi-column output.
+    # -F : Append indicator (one of */=>@|) to entries.
+    local cmd_ls='ls'
+    local -a opt_ls
+    opt_ls=('-aCF' '--color=always')
+    case "${OSTYPE}" in
+        freebsd*|darwin*)
+            if type gls > /dev/null 2>&1; then
+                cmd_ls='gls'
+            else
+                # -G : Enable colorized output.
+                opt_ls=('-aCFG')
+            fi
+            ;;
+    esac
+
+    local ls_result
+    ls_result=$(CLICOLOR_FORCE=1 COLUMNS=$COLUMNS command $cmd_ls ${opt_ls[@]} | sed $'/^\e\[[0-9;]*m$/d')
+
+    local ls_lines=$(echo "$ls_result" | wc -l | tr -d ' ')
+
+    if [ $ls_lines -gt 6 ]; then
+        echo "$ls_result" | head -n 3
+        echo '...'
+        echo "$ls_result" | tail -n 3
+        echo "$(command ls -1 -A | wc -l | tr -d ' ') files exist"
+    else
+        echo "$ls_result"
+    fi
+}
+
+
+function do_enter() {
+    if [ -n "$BUFFER" ]; then
+        zle accept-line
+        return 0
+    fi
+    echo
+    echo -e "\e[0;33m--- ls ---\e[0m"
+    # ls
+    # ↓おすすめ
+    ls_abbrev
+    if [ "$(git rev-parse --is-inside-work-tree 2> /dev/null)" = 'true' ]; then
+        echo
+        echo -e "\e[0;33m--- git status ---\e[0m"
+        git status -sb
+    fi
+    zle reset-prompt
+    return 0
+}
+zle -N do_enter
+bindkey '^m' do_enter
 
 
 # gitのトップレベルに移動
@@ -281,7 +337,8 @@ bindkey '^[d' _quote-previous-word-in-double
 exists keychain && eval `keychain --eval --agents ssh id_dsa`
 
 
-function chpwd() { ls }
+# function chpwd() { ls }
+function chpwd() { ls_abbrev }
 typeset -ga chpwd_functions
 
 function _naverc_check() {
