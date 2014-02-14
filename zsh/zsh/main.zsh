d7f1ecb4439ea77ecddc3cfb78fa236f8b869241
hamaco <hamanaka.kazuhiro@gmail.com>
2014-02-14 19:30:37 +0900

    zsh: 要らん！

--- zsh/zsh/main.zsh
+++ zsh/zsh/main.zsh
@@ -71,13 +71,6 @@ bindkey "^N" history-beginning-search-forward-end
 
 
 
-# Prompt: 画面回りの設定 ================================================== {{{1
-setopt promptsubst
-autoload -U promptinit; promptinit
-
-
-
-
 # Functions: ============================================================== {{{1
 function rationalise-dot() {
 	if [[ $LBUFFER = *.. ]]; then
@@ -122,29 +115,6 @@ function ls_abbrev() {
         echo "$ls_result"
     fi
 }
-
-
-function do_enter() {
-    if [ -n "$BUFFER" ]; then
-        zle accept-line
-        return 0
-    fi
-    echo
-    echo -e "\e[0;33m--- ls ---\e[0m"
-    # ls
-    # ↓おすすめ
-    ls_abbrev
-    if [ "$(git rev-parse --is-inside-work-tree 2> /dev/null)" = 'true' ]; then
-        echo
-        echo -e "\e[0;33m--- git status ---\e[0m"
-        git status -sb
-        echo
-    fi
-    zle reset-prompt
-    return 0
-}
-zle -N do_enter
-# bindkey '^m' do_enter
 
 
 # 上位ディレクトリを指定するcd
