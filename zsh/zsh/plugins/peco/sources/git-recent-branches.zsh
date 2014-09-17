3fbbe3d3ee73e1b09d175a0e76f0508bbea750ba
hamaco <hamanaka.kazuhiro@gmail.com>
2014-09-17 20:11:15 +0900

    zsh: perl使わないバージョンのに変えたんだ僕

--- zsh/zsh/plugins/peco/sources/git-recent-branches.zsh
+++ zsh/zsh/plugins/peco/sources/git-recent-branches.zsh
@@ -1,7 +1,9 @@
 function peco-git-recent-branches () {
-    local selected_branch=$(git for-each-ref --format='%(refname)' --sort=-committerdate refs/heads | \
-        perl -pne 's{^refs/heads/}{}' | \
-        peco --query "$LBUFFER")
+    local selected_branch=$(git for-each-ref --format='%(refname:short) | %(committerdate:relative) | %(committername) | %(subject)' --sort=-committerdate refs/heads \
+        | column -t -s '|' \
+        | peco \
+        | head -n 1 \
+        | awk '{print $1}')
     if [ -n "$selected_branch" ]; then
         BUFFER="git checkout ${selected_branch}"
         zle accept-line
@@ -11,9 +13,11 @@ function peco-git-recent-branches () {
 zle -N peco-git-recent-branches
 
 function peco-git-recent-all-branches () {
-    local selected_branch=$(git for-each-ref --format='%(refname)' --sort=-committerdate refs/heads refs/remotes | \
-        perl -pne 's{^refs/(heads|remotes)/}{}' | \
-        peco --query "$LBUFFER")
+    local selected_branch=$(git for-each-ref --format='%(refname:short) | %(committerdate:relative) | %(committername) | %(subject)' --sort=-committerdate refs/heads refs/remotes \
+        | column -t -s '|' \
+        | peco \
+        | head -n 1 \
+        | awk '{print $1}')
     if [ -n "$selected_branch" ]; then
         BUFFER="git checkout ${selected_branch}"
         zle accept-line
