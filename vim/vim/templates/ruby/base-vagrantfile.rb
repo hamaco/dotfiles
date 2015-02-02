293e7d79e799ff40173a34f833b810c7a2ae7b89
hamaco <hamanaka.kazuhiro@gmail.com>
2015-02-02 19:41:36 +0900

    vim: `nfs: true` -> `type: "nfs"` ｯﾎﾟｲ

--- vim/vim/templates/ruby/base-vagrantfile.rb
+++ vim/vim/templates/ruby/base-vagrantfile.rb
@@ -4,7 +4,7 @@ Vagrant.configure(2) do |config|
   config.vm.hostname = "{{_cursor_}}.hamaco.local"
   config.vm.network :private_network, ip: "10.10.10.10"
 
-  config.vm.synced_folder "./", "/webroot", nfs: true
+  config.vm.synced_folder "./", "/webroot", type: "nfs"
 
   config.ssh.insert_key = false
 
