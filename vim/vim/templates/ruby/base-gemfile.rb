a2a14fae28090878b6d844875e2bdc9d9427e136
hamaco <hamanaka.kazuhiro@gmail.com>
2016-07-26 23:40:00 +0900

    vim: Rubyのtemplates整理

--- vim/vim/templates/ruby/base-gemfile.rb
+++
@@ -1,3 +0,0 @@
-source "https://rubygems.org"
-
-gem "{{_cursor_}}"
--- vim/vim/templates/ruby/base-vagrantfile.rb
+++ vim/vim/templates/ruby/base-vagrantfile.rb
@@ -1,14 +1,27 @@
 Vagrant.configure(2) do |config|
-  config.vm.box = "parallels/ubuntu-14.04"
+  config.vm.box = "ubuntu/trusty64"
 
-  config.vm.hostname = "{{_cursor_}}.hamaco.local"
+  config.vm.hostname = "{{_input_:name}}.vm"
   config.vm.network :private_network, ip: "10.10.10.10"
 
-  config.vm.synced_folder "./", "/webroot", type: "nfs"
-
+  config.vm.synced_folder ".", "/webroot", type: "nfs"
   config.ssh.insert_key = false
 
+  config.vm.provider "virtualbox" do |v|
+      v.memory = 1024
+  end
+
+  if defined?(Landrush)
+    config.landrush.enabled = true
+    config.landrush.tld = 'vm'
+    config.landrush.guest_redirect_dns = false
+  end
+
   config.vm.provision :shell, :inline => <<-EOT
+    HOST_NAME="{{_input_:name}}.vm"
+    MYSQL_PASSWORD="root"
+    MYSQL_DBNAME="{{_input_:name}}_dev"
+
     aptitude update
 
     aptitude install -y git
