4bea8ffb68ca3fcd9db849a12aea58f93a5482c7
hamaco <hamanaka.kazuhiro@gmail.com>
2024-01-08 17:39:24 +0900

    gh: Add extensions

--- gh/install-extensions.sh
+++ gh/install-extensions.sh
@@ -3,3 +3,6 @@
 gh extension install k1LoW/gh-grep
 gh extension install kawarimidoll/gh-graph
 gh extension install kawarimidoll/gh-q
+gh extension install yusukebe/gh-markdown-preview
+gh extension install seachicken/gh-poi
+gh extension install dlvhdr/gh-dash
