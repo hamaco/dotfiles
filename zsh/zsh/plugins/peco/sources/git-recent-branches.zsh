fd01cd7b636641ed1632e273eb03dcf132d32c1a
hamaco <hamanaka.kazuhiro@gmail.com>
2025-01-15 11:00:27 +0900

    zsh: git-recent-branchesで自分以外のブランチは赤色表示

--- zsh/zsh/plugins/peco/sources/git-recent-branches.zsh
+++ zsh/zsh/plugins/peco/sources/git-recent-branches.zsh
@@ -1,5 +1,12 @@
+user_name=$(git config user.name)
+fmt="\
+%(if:equals=$user_name)%(authorname)%(then)%(color:default)%(else)%(color:brightred)%(end)%(refname:short)|\
+%(committerdate:relative)|\
+%(committername)|\
+%(subject)"
+
 function fzf-git-recent-branches () {
-    local selected_branch=$(git for-each-ref --format='%(refname:short) | %(committerdate:relative) | %(committername) | %(subject)' --sort=-committerdate refs/heads \
+    local selected_branch=$(git for-each-ref --format=$fmt --sort=-committerdate --color=always refs/heads \
         | column -t -s '|' \
         | fzf-tmux -p80% \
         | head -n 1 \
@@ -14,7 +21,7 @@ function fzf-git-recent-branches () {
 zle -N fzf-git-recent-branches
 
 function fzf-git-recent-all-branches () {
-    local selected_branch=$(git for-each-ref --format='%(refname:short) | %(committerdate:relative) | %(committername) | %(subject)' --sort=-committerdate refs/heads refs/remotes \
+    local selected_branch=$(git for-each-ref --format=$fmt --sort=-committerdate --color=always refs/heads refs/remotes \
         | column -t -s '|' \
         | fzf-tmux -p80% \
         | head -n 1 \
