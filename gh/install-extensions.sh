b5eae25f404346adc0d18f77364c7b3702bf59fa
hamaco <hamanaka.kazuhiro@gmail.com>
2025-12-29 00:03:35 +0900

    gh: gh-furik

--- gh/install-extensions.sh
+++ gh/install-extensions.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
+gh extension install dlvhdr/gh-dash
 gh extension install k1LoW/gh-grep
 gh extension install k1LoW/gh-triage
 gh extension install kawarimidoll/gh-graph
 gh extension install kawarimidoll/gh-q
-gh extension install yusukebe/gh-markdown-preview
+gh extension install kenchan/gh-furik
 gh extension install seachicken/gh-poi
-gh extension install dlvhdr/gh-dash
+gh extension install yusukebe/gh-markdown-preview
