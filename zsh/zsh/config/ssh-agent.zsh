a35e6aa26c72f9dcbebbd9be4751945a2de2a5d5
hamaco <hamanaka.kazuhiro@gmail.com>
2026-01-03 02:42:29 +0900

    zsh: 1Password SSH Agentを使うようにした

--- zsh/zsh/config/ssh-agent.zsh
+++ zsh/zsh/config/ssh-agent.zsh
@@ -1,25 +1,21 @@
 ## SSHのagent forward
-if exists keychain; then
-    eval `keychain --eval id_ed25519 id_rsa old.id_rsa`
-else
-    echo -e "\e[31mkeychainコマンドインストールしてください。 (brew install keychain)\e[m"
-fi
-
-## Windows で 1Password for SSH を使う場合はこっちをコメントアウトする
-# if [[ "$(uname -r)" == *microsoft* ]]; then
-#     ## SSH for 1Password SSH
-#     # via. https://stuartleeks.com/posts/wsl-ssh-key-forward-to-windows
-#     export SSH_AUTH_SOCK=$HOME/.ssh/agent.sock
-#     ALREADY_RUNNING=$(ps -auxww | grep -q "[n]piperelay.exe -ei -s //./pipe/openssh-ssh-agent"; echo $?)
-#     if [[ $ALREADY_RUNNING != "0" ]]; then
-#         if [[ -S $SSH_AUTH_SOCK ]]; then
-#             echo "removing previous socket..."
-#             rm $SSH_AUTH_SOCK
-#         fi
-#     
-#         echo "Starting SSH-Agent relay..."
-#     
-#         (setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"/mnt/c/Users/hamaco/go/bin/npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork &) >/dev/null 2>&1
-#     fi
+# if exists keychain; then
+#     eval `keychain --eval id_ed25519 id_rsa old.id_rsa`
+# else
+#     echo -e "\e[31mkeychainコマンドインストールしてください。 (brew install keychain)\e[m"
 # fi
 
+# 1Password SSH Agent
+export SSH_AUTH_SOCK="$HOME/.ssh/agent.sock"
+if [[ -S ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock ]]; then
+    # for macOS
+    ln -sf ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock ~/.ssh/agent.sock
+elif exists socat && exists npiperelay.exe; then
+    # for Windows WSL
+    if ! ss -a | grep -q "$SSH_AUTH_SOCK"; then
+      rm -f "$SSH_AUTH_SOCK"
+      (setsid socat UNIX-LISTEN:"$SSH_AUTH_SOCK",fork EXEC:"npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork &) >/dev/null 2>&1
+    fi
+else
+    echo -e "\e[31m1Password for SSH が有効化されていません\e[m"
+fi
